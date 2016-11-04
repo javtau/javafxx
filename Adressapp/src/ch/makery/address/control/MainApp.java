0b63723ac09227d7439d6454d19bb890e77182a1
javi0 <javier.lozano.almeda@gmail.com>
2016-11-04 09:18:55 +0100

    paso 2 final

--- Adressapp/src/ch/makery/address/control/MainApp.java
+++ Adressapp/src/ch/makery/address/control/MainApp.java
@@ -85,7 +85,7 @@ public class MainApp extends Application {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            loader.setLocation(MainApp.class.getResource("../view/PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             // Set person overview into the center of root layout.
